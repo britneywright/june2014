1054db9defd82093d779a3eb74273740e052f51e
Britney Wright <blw06g@gmail.com>
2014-06-11 22:51:54 -0400

    another caesar_cipher refactor

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -2,20 +2,18 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 def shift_letter(letter,shift_number)
-  letter_value = ALPHABET_VALUES[letter.downcase] + shift_number
-  reversed_hash = ALPHABET_VALUES.invert
-  new_letter = reversed_hash[letter_value % 26]
+  letter_value = ALPHABET.find_index(letter.downcase) + shift_number
+  new_letter = ALPHABET[letter_value % 26]
   letter == letter.upcase ? letter.replace(new_letter.upcase) : letter.replace(new_letter)
 end
 
-alphabet = ("a".."z").to_a
-ALPHABET_VALUES = Hash[alphabet.each_with_index.map {|value, index| [value, index]}]
-
 def caesar_cipher(phrase, shift_number)
   phrase.gsub(/\w/) do |letter| 
-    shift_letter(letter,shift_number)
+   shift_letter(letter,shift_number)
   end
 end
+
+ALPHABET = ("a".."z").to_a
 
 describe "caesar cipher shifts the letters in text by a specified number" do
   it "should shifts all letters by 5 letters" do
--- stock_picker.rb
+++
@@ -1,5 +0,0 @@
-def stock_picker(args)
-    args(2)
-end
-
-stock_picker([2,4,20,5])
