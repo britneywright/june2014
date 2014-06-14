9abccea19f9e11073213cef9cef86685702e7677
Britney Wright <blw06g@gmail.com>
2014-06-14 14:48:51 -0400

    completed first version of stock_picker

---
+++ stock_picker.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+
+def stock_picker(days)
+  best_days = []
+  days.each do |buy|
+    days.each do |sell|
+      profit = sell - buy
+      if days.find_index(sell) > days.find_index(buy)
+        best_days << [days.index(buy), days.index(sell), profit]
+      end
+    end
+  end
+  highest_profit = best_days.max_by{ |buy,sell, profit| profit }
+  highest_profit.pop
+  highest_profit
+end
+
+describe "given a number of days with different stock values return the best day to buy and the best day to sell" do
+  it "should display the second day and the fifth day" do
+    stock_picker([20, 4, 5, 7, 12]).must_equal [1, 4]
+  end
+  it "should display the first day and the second day" do
+    stock_picker([3, 30, 5, 7, 12]).must_equal [0, 1]
+  end
+  it "should display the first day and the fourth day" do
+    stock_picker([2, 4, 5, 7, 1]).must_equal [0, 3]
+  end    
+end
