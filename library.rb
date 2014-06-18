8c00bc746cc3f64b857639eb8ee5b4794d0f4e5e
Britney Wright <blw06g@gmail.com>
2014-06-17 23:52:34 -0400

    update sort.rb

--- library.rb
+++ library.rb
@@ -2,9 +2,3 @@ class Book
   def initialize(title, categoy, borrow_time)
   end
 end
-
-result = []
-first_names = ["Hamburglar", "Grimace", "Ronald"]
-last_name = "McDonald"
-
-print first_names.map {|first_name| first_name + " " + last_name}
--- sort.rb
+++ sort.rb
@@ -1,19 +1,23 @@
 require 'minitest/autorun'
 
-def handmade_sort(arr)
-  arr.collect! do |number|
-    result = number <=> arr.at(arr.index(number).next)
-    if result == 1 || result == 0
-      arr.at(arr.index(number).next)
-    else
-      number
+def bubble_sort(arr)
+  arr.each_index do |index|
+    comparison = index <=> arr[index + 1]
+    if comparison == 1
+      a = arr[index]
+      b = arr[index + 1]
+      arr[index] = b
+      arr[index + 1] = a
     end
   end
-  print arr
+  arr
 end
 
 describe "sort an array of objects" do
   it "should sort number from smallest to largest" do
-    handmade_sort([3, 2, 6, 12, 4, 9]).must_equal [2, 3, 4, 6, 9, 12]
+    bubble_sort([1, 2, 3]).must_equal [1, 2, 3]
+  end  
+  it "should sort number from smallest to largest" do
+    bubble_sort([3, 2, 6, 12, 4, 9]).must_equal [2, 3, 4, 6, 9, 12]
   end
 end
