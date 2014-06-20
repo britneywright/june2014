9a16cde37fd1777aeb38d654e46b16641d4dcf5d
Britney Wright <blw06g@gmail.com>
2014-06-19 23:09:06 -0400

    enumberable module and tdd

--- library.rb
+++ library.rb
@@ -2,3 +2,5 @@ class Book
   def initialize(title, categoy, borrow_time)
   end
 end
+
+puts [1,2,3].inject(:*)
---
+++ my_enumerables.rb
@@ -0,0 +1,37 @@
+module Enumerable
+
+  def my_each
+    for num in self
+      num
+    end
+  end
+
+  def my_each_with_index
+  end
+
+  def my_select
+  end
+
+  def my_all?
+  end
+
+  def my_any?
+  end
+
+  def my_none?
+  end
+
+  def my_count
+  end
+
+  def my_map
+  end
+
+  def my_inject
+  end
+end
+
+def multiply_els(arr)
+end
+
+print [1,2,3].my_each
